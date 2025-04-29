0c7680138e9faf907d4b9b7838e8e00b4c86ba3e
Tarun Velagala <tarunvelagala80@gmail.com>
2025-04-29 15:51:19 +0530

    Time: 0 ms (100%), Space: 41.8 MB (26.79%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,35 +1,50 @@
 class Solution {
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-	    int row = matrix.length;
-	    int col = matrix[0].length;
-	    
-	    ArrayList<Integer> res = new ArrayList<>();
-	    int top = 0;
-	    int bottom = row - 1;
-	    int left = 0;
-	    int right = col - 1;
+        // Initialize row and columns
+        int row = matrix.length;
+        int col = matrix[0].length;
 
+        ArrayList<Integer> res = new ArrayList<>();
+
+        // Initialize top, bottom, left, right
+        int top = 0;
+        int bottom = row - 1;
+        int left = 0;
+        int right = col - 1;
+
+        // While top <= bottom and left <= right 
         while (left <= right && top <= bottom) {
+
+            // Traverse from left -> right 
+            // Increment the top 
             for (int i = left; i <= right; i++) {
                 res.add(matrix[top][i]);
             }
             top++;
 
-            for (int i = top; i <= bottom;i++) {
+            // Traverse from top to bottom
+            // Decrement the right
+            for (int i = top; i <= bottom; i++) {
                 res.add(matrix[i][right]);
             }
             right--;
 
+            // Every time check on spiral check if 
+            // top exceeds bottom and print right to left (reverse order)
+            // and decrement bottom. 
             if (top <= bottom) {
-                for (int i = right; i >= left;i--) {
+                for (int i = right; i >= left; i--) {
                     res.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
 
+            // Every time check on spiral check if 
+            // left exceeds right and print bottom to top (reverse order)
+            // and increment left. 
             if (left <= right) {
-                for (int i = bottom; i >= top;i--) {
+                for (int i = bottom; i >= top; i--) {
                     res.add(matrix[i][left]);
                 }
                 left++;

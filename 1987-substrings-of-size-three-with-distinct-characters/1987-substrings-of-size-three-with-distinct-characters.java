becb91736a5059bb2a2bfb8168f1d6648ace7067
Tarun Velagala <tarunvelagala80@gmail.com>
2025-04-30 20:14:32 +0530

    Time: 3 ms (38.49%), Space: 42.8 MB (24%) - LeetHub

---
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int countGoodSubstrings(String s) {
+        if (s.length() < 3) { //  if the input string is less than 3, return 0
+            return 0;
+        }
+        int count = 0;
+        Set<Character> mySet = new HashSet<>(); // use set to store unique characters
+        int start = 0, end = 3;
+
+        String sub = s.substring(start, end);
+        for (int i = 0; i < 3; i++) {
+            mySet.add(sub.charAt(i));
+        }
+        if (mySet.size() == 3) count++;
+
+        for (end++, start++; end < s.length() + 1; start++, end++) {
+            mySet.clear(); // reset the set
+            sub = s.substring(start, end);
+            for (int k = 0; k < 3; k++) {
+                mySet.add(sub.charAt(k));
+            }
+            if (mySet.size() == 3) count++;
+        }
+        return count;
+    }
+}

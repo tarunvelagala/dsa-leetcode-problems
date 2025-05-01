db75135d53e7bde5b701c6c1c30a57109e1453a5
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-01 17:40:26 +0530

    Time: 1 ms (99.59%), Space: 57.3 MB (21.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,8 +1,7 @@
 class Solution {
-
     public int maxSubArrayBruteForce(int[] nums) {
         // Base Case
-        if (nums.length == 1) {
+        if(nums.length == 1) {
             return nums[0];
         }
         int max = Integer.MIN_VALUE;
@@ -10,7 +9,7 @@ class Solution {
             int sum = 0;
             for (int j = i; j < nums.length; j++) {
                 sum += nums[j];
-                max = Math.max(max, sum);
+                max = Math.max(max, sum);   
             }
         }
         return max;
@@ -19,17 +18,17 @@ class Solution {
     // Implemented using K'Danes Algorithm.
     public int maxSubArray(int[] nums) {
         // Base Case
-        if (nums.length == 1) {
+        if(nums.length == 1) {
             return nums[0];
         }
 
         int currSum = 0;
         int globalSum = Integer.MIN_VALUE;
 
-        for (int i = 0; i < nums.length; i++) {
+        for(int i = 0;i<nums.length;i++) {
             currSum += nums[i];
             globalSum = Math.max(globalSum, currSum);
-            if (currSum < 0) {
+            if(currSum < 0) {
                 currSum = 0;
             }
         }

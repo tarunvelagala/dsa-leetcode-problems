c5555b42cfbecd746a162a906f690e058c79694e
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-01 14:26:55 +0530

    Time: 0 ms (100%), Space: 41.9 MB (82.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -5,24 +5,50 @@ class Solution {
         Arrays.sort(nums);
     }
 
+    // Better solution using count variables and loops.
     public void sortColorsBetter(int[] nums) {
         int count0 = 0;
         int count1 = 0;
         int count2 = 0;
 
-        for(int i = 0;i<nums.length;i++) {
-            if(nums[i] == 0) count0++;
-            else if(nums[i] == 1) count1++;
-            else count2++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) count0++; else if (nums[i] == 1) count1++; else count2++;
         }
 
-        for(int i = 0;i<count0;i++) nums[i] = 0;
-        for(int i = count0;i<count0+count1;i++) nums[i] = 1;
-        for(int i = count0+count1;i<nums.length;i++) nums[i] = 2;
+        for (int i = 0; i < count0; i++) nums[i] = 0;
+        for (int i = count0; i < count0 + count1; i++) nums[i] = 1;
+        for (int i = count0 + count1; i < nums.length; i++) nums[i] = 2;
+    }
+
+    // Using Dutch National Flag Algorithm
+    // https://github.com/tarunvelagala/dsa-leetcode-problems/blob/main/_patterns/algorithms/DutchNationalFlag.md
+    public void sortColorsOptimal(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) mid++; else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
     }
 
     public void sortColors(int[] nums) {
-       //  sortColorsBruteForce(nums); // Causes TLE
-        sortColorsBetter(nums);
+        // Causes TLE
+        //  sortColorsBruteForce(nums);
+        // sortColorsBetter(nums);
+
+        // Dutch National Flag Algorithm
+        sortColorsOptimal(nums);
     }
 }

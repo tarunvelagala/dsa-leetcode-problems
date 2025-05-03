255a4d4f19079860beebaba68667433614265dfc
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-03 09:52:23 +0530

    Time: 19 ms (48.01%), Space: 45 MB (35.19%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -4,37 +4,55 @@ class Solution {
         return "aeiou".indexOf(c) != -1;
     }
 
+    // Bruteforce approach
+    public int maxVowelsBruteForce(String s, int k) {
+        int maxNumberOfVowels = 0;
+        int currentNumberOfVowels = 0;
+
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            currentNumberOfVowels = 0;
+            for (int j = i; j < i + k; j++) {
+                if (isVowel(s.charAt(j))) {
+                    currentNumberOfVowels++;
+                }
+            }
+            maxNumberOfVowels = Math.max(maxNumberOfVowels, currentNumberOfVowels);
+        }
+        return maxNumberOfVowels;
+    }
+
+    // Fixed Size sliding window of size k.
     public int maxVowels(String s, int k) {
         // Base Case
         if (s.length() < 0) {
             return 0;
         }
 
-        int maxVowelCount = 0;
-        int currVowelCount = 0;
+        int maxNumberOfVowels = 0;
+        int currentNumberOfVowels = 0;
 
         // Initialize the sliding window of size k
         for (int i = 0; i < k; i++) {
             if (isVowel(s.charAt(i))) {
-                currVowelCount++;
+                currentNumberOfVowels++;
             }
         }
-        maxVowelCount = currVowelCount;
+        maxNumberOfVowels = currentNumberOfVowels;
 
         // For remaining n elements i.e k -> n
         // if s[i] is a vowel increment the count
         // if s[i-k] is vowel decrement the count
         for (int i = k; i < s.length(); i++) {
             if (isVowel(s.charAt(i))) {
-                currVowelCount++;
+                currentNumberOfVowels++;
             }
             if (isVowel(s.charAt(i - k))) {
-                currVowelCount--;
+                currentNumberOfVowels--;
             }
 
             // Calculate the maxVowelCount
-            maxVowelCount = Math.max(maxVowelCount, currVowelCount);
+            maxNumberOfVowels = Math.max(maxNumberOfVowels, currentNumberOfVowels);
         }
-        return maxVowelCount;
+        return maxNumberOfVowels;
     }
 }

84e4ea2d7822f4face8c7e1a87e0e264f9178754
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-04 10:59:02 +0530

    Time: 1 ms (91.27%), Space: 41.6 MB (70.45%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -9,7 +9,10 @@ class Solution {
         int count = 0;
 
         for (int i = 0; i < s.length() - 3 + 1; i++) {
-            if (s.charAt(i + 1) != s.charAt(i) && s.charAt(i + 2) != s.charAt(i) && s.charAt(i + 2) != s.charAt(i + 1)) {
+            char a = s.charAt(i);
+            char b = s.charAt(i + 1);
+            char c = s.charAt(i + 2);
+            if (a != b && b != c && c != a) {
                 count++;
             }
         }

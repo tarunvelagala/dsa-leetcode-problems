32f3c037c16768b90b1da47dc2dcf9cd07fbbd7d
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-05 14:52:19 +0530

    Time: 32 ms (47.28%), Space: 45 MB (46.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,21 +1,39 @@
 class Solution {
 
     // Generate all the arrays of i, j -> i, i+1
-    public int[] twoSum(int[] nums, int target) {
+    // Uses a HashMap to store the elements and its index
+    public int[] twoSumBetter(int[] nums, int target) {
         int n = nums.length;
         int[] ans = new int[2];
         ans[0] = -1;
         ans[1] = -1;
-        HashMap<Integer, Integer> x = new HashMap<>();
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int num = nums[i];
             int complement = target - num;
-            if (x.containsKey(complement)) {
-                ans[0] = x.get(complement);
+            if (indexMap.containsKey(complement)) {
+                ans[0] = indexMap.get(complement);
                 ans[1] = i;
                 return ans;
             }
-            x.put(nums[i], i);
+            indexMap.put(nums[i], i);
+        }
+        return ans;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                if (num + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
         }
         return ans;
     }

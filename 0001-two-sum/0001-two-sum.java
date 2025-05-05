d922e6df864420fa72d20f0e595e0c33e3f85f52
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-05 14:55:26 +0530

    Time: 32 ms (47.28%), Space: 44.8 MB (78.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,8 +5,7 @@ class Solution {
     public int[] twoSumBetter(int[] nums, int target) {
         HashMap<Integer, Integer> indexMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int complement = target - num;
+            int complement = target - nums[i];
             if (indexMap.containsKey(complement)) {
                 return new int[] { i, indexMap.get(complement) };
             }

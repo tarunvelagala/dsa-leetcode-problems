63f704a986e1288e60e29e4f5b590ca2ee82ae2b
Tarun Velagala <tarunvelagala80@gmail.com>
2025-06-06 16:14:35 +0530

    Time: 28 ms (80.38%), Space: 51.4 MB (71.63%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,27 @@
 class Solution {
 
-    public List<List<Integer>> threeSumO(int[] nums) {
+    public List<List<Integer>> threeSumBF(int[] nums) {
         // Generate all sub arrays
-        // nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k] &&
         // nums[i] + nums[j] + nums[k] = 0
-        List<List<Integer>> res = new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0 && nums[i] != nums[j] && nums[j] != nums[k] && nums[k] != nums[i]) {
-                        List<Integer> triplets = List.of(nums[i], nums[j], nums[k]);
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplets = Arrays.asList(nums[i], nums[j], nums[k]);
+                        triplets.sort(null);
                         res.add(triplets);
                     }
                 }
             }
         }
-        return res;
+        return new ArrayList<>(res);
     }
 
     public List<List<Integer>> threeSumBetter(int[] nums) {
         final Set<List<Integer>> result = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
-            Set<Integer> numberIndexSet = new HashSet<>();
+            final Set<Integer> numberIndexSet = new HashSet<>();
             for (int j = i + 1; j < nums.length; j++) {
                 int complement = -(nums[i] + nums[j]);
                 if (numberIndexSet.contains(complement)) {

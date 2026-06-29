fa3340540faa4c1d424438853ce3b1d4c7c58e2c
Tarun Velagala <tarunvelagala80@gmail.com>
2026-06-29 20:46:45 +0530

    Time: 2 ms (99.37%), Space: 47.3 MB (10.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -9,15 +9,17 @@ class Solution {
         // C.O.D.E - 
         // T.E.S.T - 
 
-        if(nums == null || nums.length == 0) return new int[]{-1, -1};
+        if (nums == null || nums.length == 0)
+            return new int[] { -1, -1 };
         final Map<Integer, Integer> complementMap = new HashMap<Integer, Integer>();
-        for(int i = 0;i<nums.length;i++) {
+        for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-            if(complementMap.containsKey(complement)) {
-                return new int[]{i, complementMap.getOrDefault(complement, 0)};
+            if (complementMap.containsKey(complement)) {
+                return new int[] { i, complementMap.getOrDefault(complement, 0) };
+            } else {
+                complementMap.put(nums[i], i);
             }
-            complementMap.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+        return new int[] { -1, -1 };
     }
 }
